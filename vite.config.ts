34cd3ceec80d35c76346d16e8b625756658d7977
Daniel <danieldjamilui@gmail.com>
2026-01-09 00:02:19 +0700

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: 'https://github.com/danieldjamilui-ux',
+  base: '/Portofolio/',
   plugins: [
     react(),
     tailwindcss(),
